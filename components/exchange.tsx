462cab84dda23a969ca5f268f527cf87794c2501
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-01-08 19:03:52 +0200

    fix

--- components/exchange.tsx
+++ components/exchange.tsx
@@ -353,7 +353,7 @@ export default function Exchange({
             )}
             {showCityForm ? (
               <div className='flex flex-col w-full justify-around gap-[20px] md:flex-row mb-[30px] max-h-[620px]'>
-                <div className='md:h-[300px]'>
+                <div className='md:h-[300px] overflow-y-auto'>
                   <div className='md:hidden'>
                     <h3 className='text-[24px] text-white leading-[28px] font-bold m-w-[33.33%] pl-[32px] mb-[30px] font-raleway'>
                       Откуда:
@@ -403,7 +403,7 @@ export default function Exchange({
                     />
                   </div>
                 </div>
-                <div className='md:h-[300px]'>
+                <div className='md:h-[300px] overflow-y-auto'>
                   <div className='md:hidden'>
                     <h3 className='text-[24px] text-white leading-[28px] font-bold m-w-[33.33%] pl-[32px] mb-[30px] font-raleway'>
                       Куда:
