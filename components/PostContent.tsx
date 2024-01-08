3e25e0be1c4675b159829fbff3e423eee28c47cf
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-01-08 18:04:11 +0200

    add fonts

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -2,7 +2,7 @@
 
 // import dynamic from "next/dynamic";
 import ReactQuill from "react-quill";
-import "react-quill/dist/quill.bubble.css";
+// import "react-quill/dist/quill.bubble.css";
 // const ReactQuill = dynamic(import("react-quill"), { ssr: false });
 export default function PostContent({ content }: { content: any }) {
   return <ReactQuill value={content} readOnly={true} theme={"bubble"} />;
