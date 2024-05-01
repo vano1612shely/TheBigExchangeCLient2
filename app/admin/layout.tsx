5121858ec05c1ec8a9164917a19d8df647cbd2ef
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-05-01 11:09:35 +0300

    123

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,22 +1,15 @@
 "use client";
-
-import type { Metadata } from "next";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import localFont from "next/font/local";
-import { Raleway } from "next/font/google";
-import Container from "@/components/container";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "@/store/store";
-
+import "../globals.css";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang='ru'>
+    <html lang="ru">
       <body style={{ fontFamily: "Raleway,sans-serif" }}>
         <Provider store={store}>
           <PersistGate persistor={persistor} loading={null}>
