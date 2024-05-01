bfcc69b60d9f1d395daa21ec7bc674b616522dfc
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-05-01 17:12:36 +0300

    123

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { redirect } from "next/navigation";
 import { locales } from "../config";
+import { useEffect } from "react";
 
 export default function RootPage() {
-  let language = navigator.language;
-  // @ts-ignore
-  if (locales.includes(language)) {
-    redirect("/" + language);
-  } else {
-    redirect("/uk");
-  }
+  useEffect(() => {
+    let language = navigator.language;
+    // @ts-ignore
+    if (locales.includes(language)) {
+      redirect("/" + language);
+    } else {
+      redirect("/uk");
+    }
+  }, []);
 }
