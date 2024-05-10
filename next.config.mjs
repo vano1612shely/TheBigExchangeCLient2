fcee8a2ed374a30b095be6624c35f6c5fa175941
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-05-10 18:36:21 +0300

    123

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-const createNextIntlPlugin = require("next-intl/plugin");
+import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
