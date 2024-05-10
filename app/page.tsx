691cfbb63e1806419441b585684f9762615640e0
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-05-10 21:32:49 +0300

    123

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { redirect } from "next/navigation";
+import MainPage from "../components/MainPage";
+import React from "react";
 
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
   redirect("/en");
+
+  return <MainPage />;
 }
