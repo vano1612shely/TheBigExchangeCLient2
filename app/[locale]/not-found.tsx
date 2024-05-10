0591bcff62277f2d604b3c79e1aa3e33832b9ef8
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-05-10 18:47:29 +0300

    123

--- app/[locale]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect } from "react";
-export const dynamic = "force-dynamic";
-export default function NotFound() {
-  useEffect(() => {
-    window.location.reload();
-  }, []);
-  return;
-}
