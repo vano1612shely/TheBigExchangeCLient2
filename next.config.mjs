0ffef5fdd3f2cb1771d0d69b8ce16d2c81b55950
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-05-10 18:34:54 +0300

    123

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,6 @@ const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/robots.txt",
-        destination: "/api/robots",
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
