b23b557024abd3e8f29d8f7f9852f09ba645fa80
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-05-10 19:05:14 +0300

    123

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Error from "next/error";
-import React from "react";
+import React, { useEffect } from "react";
 
 // Render the default Next.js 404 page when a route
 // is requested that doesn't match the middleware and
 // therefore doesn't have a locale associated with it.
 
 export default function NotFound() {
-  window.location.reload();
+  useEffect(() => {
+    window.location.reload();
+  }, []);
 }
