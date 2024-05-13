799d279314a5edb3de1aac503dbec671b2dea9f3
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-05-13 14:33:37 +0300

    123

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import LanguageProvider from "../components/LanguageProvider";
+import Head from "next/head";
 export default function RootPage() {
   return (
     <LanguageProvider>
+      <Head>
+        <title>The Big Exchange</title>
+        <meta
+          name="description"
+          content="TheBigExchange - мгновенный обмен криптовалют в Украине. Низкие комиссии, безопасность, поддержка 24/7. Просто, быстро, надежно!"
+        />
+        <meta property="og:title" content="The Big Exchange" />
+        <meta
+          property="og:description"
+          content="TheBigExchange - мгновенный обмен криптовалют в Украине. Низкие комиссии, безопасность, поддержка 24/7. Просто, быстро, надежно!"
+        />
+        <meta
+          property="og:image"
+          content="https://thebigexchange.net/icon.png"
+        />
+      </Head>
       <h1>
         TheBigExchange - мгновенный обмен криптовалют в Украине. Низкие
         комиссии, безопасность, поддержка 24/7. Просто, быстро, надежно!
